a584b337933293400c7544b0542a22f661e09a4b
chhandakguria <chhandakguria@gmail.com>
2022-05-19 01:03:35 +0530

    "Changes are committed"

--- Hello.go
+++ Hello.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/chhandakguria/GoAPIs/Packages/Calc"
+	"github.com/chhandakguria/GoAPIs/Packages/Numbers"
 )
 
 func main() {
 
-	fmt.Println("Result: ", Calculator(10, 5, '/'))
-	fmt.Println("Staus: ", IsEven(40))
+	fmt.Println("Result: ", Calc.Calculator(10, 5, '/'))
+	fmt.Println("Staus: ", Numbers.IsEven(40))
 
 }
